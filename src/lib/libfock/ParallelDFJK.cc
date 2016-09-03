28e2cff5225f392e59f423b33353a06d0abd4298
Kevin Hannon <kannon1992@gmail.com>
2016-09-03 14:49:31 -0400

    Speeding up ParallelDFJK by avoiding paralleization of DF metric.

--- src/lib/libfock/ParallelDFJK.cc
+++ src/lib/libfock/ParallelDFJK.cc
@@ -68,71 +68,76 @@ void ParallelDFJK::J_one_half()
     dims[0] = naux;
     dims[1] = naux;
     chunk[0] = -1;
-    chunk[1] = naux;
+    chunk[1] = -1;
     J_12_GA_ = NGA_Create(C_DBL, 2, dims, (char *)"J_1/2", chunk);
     if(not J_12_GA_)
         throw PSIEXCEPTION("Failure in creating J_^(-1/2) in GA");
 
-    boost::shared_ptr<IntegralFactory> Jfactory(new IntegralFactory(auxiliary_, BasisSet::zero_ao_basis_set(), auxiliary_, BasisSet::zero_ao_basis_set()));
-    std::vector<boost::shared_ptr<TwoBodyAOInt> > Jeri;
-    for (int thread = 0; thread < nthread; thread++) {
-        Jeri.push_back(boost::shared_ptr<TwoBodyAOInt>(Jfactory->eri()));
-    }
-
-    std::vector<std::pair<int, int> > Jpairs;
-    for (int M = 0; M < auxiliary_->nshell(); M++) {
-        for (int N = 0; N <= M; N++) {
-            Jpairs.push_back(std::pair<int,int>(M,N));
-        }
-    }
-    long int num_Jpairs = Jpairs.size();
-
-    #pragma omp parallel for schedule(dynamic) num_threads(nthread)
-    for (long int PQ = 0L; PQ < num_Jpairs; PQ++) {
-
-        int thread = 0;
-        #ifdef _OPENMP
-            thread = omp_get_thread_num();
-        #endif
-
-        std::pair<int,int> pair = Jpairs[PQ];
-        int P = pair.first;
-        int Q = pair.second;
-
-        Jeri[thread]->compute_shell(P,0,Q,0);
-
-        int np = auxiliary_->shell(P).nfunction();
-        int op = auxiliary_->shell(P).function_index();
-        int nq = auxiliary_->shell(Q).nfunction();
-        int oq = auxiliary_->shell(Q).function_index();
-
-        const double* buffer = Jeri[thread]->buffer();
-
-        for (int p = 0; p < np; p++) {
-        for (int q = 0; q < nq; q++) {
-            Jp[p + op][q + oq] =
-            Jp[q + oq][p + op] =
-                (*buffer++);
-        }}
-    }
-    Jfactory.reset();
-    Jeri.clear();
-
-    // > Invert J < //
-
-    J->power(-1.0/2.0, 1e-10);
-    outfile->Printf("\n JRMS: %8.8f", J->rms());
     if(GA_Nodeid() == 0)
     {
-        for(int me = 0; me < GA_Nnodes(); me++)
-        {
-            int begin_offset[2];
-            int end_offset[2];
-            NGA_Distribution(J_12_GA_, me, begin_offset, end_offset);
-            int offset = begin_offset[0];
-            NGA_Put(J_12_GA_, begin_offset, end_offset, J->pointer()[offset], &naux);
+        boost::shared_ptr<IntegralFactory> Jfactory(new IntegralFactory(auxiliary_, BasisSet::zero_ao_basis_set(), auxiliary_, BasisSet::zero_ao_basis_set()));
+        std::vector<boost::shared_ptr<TwoBodyAOInt> > Jeri;
+        for (int thread = 0; thread < nthread; thread++) {
+            Jeri.push_back(boost::shared_ptr<TwoBodyAOInt>(Jfactory->eri()));
         }
+
+        std::vector<std::pair<int, int> > Jpairs;
+        for (int M = 0; M < auxiliary_->nshell(); M++) {
+            for (int N = 0; N <= M; N++) {
+                Jpairs.push_back(std::pair<int,int>(M,N));
+            }
+        }
+        long int num_Jpairs = Jpairs.size();
+
+        #pragma omp parallel for schedule(dynamic) num_threads(nthread)
+        for (long int PQ = 0L; PQ < num_Jpairs; PQ++) {
+
+            int thread = 0;
+            #ifdef _OPENMP
+                thread = omp_get_thread_num();
+            #endif
+
+            std::pair<int,int> pair = Jpairs[PQ];
+            int P = pair.first;
+            int Q = pair.second;
+
+            Jeri[thread]->compute_shell(P,0,Q,0);
+
+            int np = auxiliary_->shell(P).nfunction();
+            int op = auxiliary_->shell(P).function_index();
+            int nq = auxiliary_->shell(Q).nfunction();
+            int oq = auxiliary_->shell(Q).function_index();
+
+            const double* buffer = Jeri[thread]->buffer();
+
+            for (int p = 0; p < np; p++) {
+            for (int q = 0; q < nq; q++) {
+                Jp[p + op][q + oq] =
+                Jp[q + oq][p + op] =
+                    (*buffer++);
+            }}
+        }
+        Jfactory.reset();
+        Jeri.clear();
+
+        // > Invert J < //
+
+        J->power(-1.0/2.0, 1e-10);
+        outfile->Printf("\n JRMS: %8.8f", J->rms());
+        //for(int me = 0; me < GA_Nnodes(); me++)
+        //{
+        int begin_offset[2];
+        int end_offset[2];
+        //NGA_Distribution(J_12_GA_, me, begin_offset, end_offset);
+        begin_offset[0] = 0;
+        begin_offset[1] = 0;
+        end_offset[0]   = naux - 1;
+        end_offset[1]   = naux - 1;
+        int offset = begin_offset[0];
+        NGA_Put(J_12_GA_, begin_offset, end_offset, J->pointer()[0], &naux);
+       // }
     }
+
 }
 void ParallelDFJK::compute_qmn()
 {
